3765bd944aa0422365b485a86e7148b903c5c910
gabriel-fogaca <l44.gabriel@hotmail.com>
2024-03-27 23:05:09 -0300

    React Rotas

---
+++ src/Routes/NotFound/NotFoundBody/index.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export default function NotFoundBody() {
+    return (
+        <div className='container'>
+            <h2 className='mt20'>Não encontrado!</h2>
+        </div>
+    );
+}
